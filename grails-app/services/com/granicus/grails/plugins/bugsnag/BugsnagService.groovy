dc784227b0cbac795f536384fe4390cb8fb9555f
karina_ordonez_flores <kordonez@mantano.com>
2022-10-26 14:44:45 +0100

    1 version website

--- grails-app/services/com/granicus/grails/plugins/bugsnag/BugsnagService.groovy
+++ grails-app/services/com/granicus/grails/plugins/bugsnag/BugsnagService.groovy
@@ -1,7 +1,7 @@
 package com.granicus.grails.plugins.bugsnag
 
-import com.bugsnag.Client
-import com.bugsnag.MetaData
+import com.bugsnag.Bugsnag
+import com.bugsnag.Report
 import grails.util.Environment
 import grails.web.context.ServletContextHolder as SCH
 
@@ -11,9 +11,7 @@ class BugsnagService {
 
     def grailsApplication
 
-    def addMetadata = null
-
-    def getConfiguredClient( context ) {
+    def getConfiguredClient(  ) {
 
         log.info "getConfiguredClient()"
 
@@ -32,85 +30,70 @@ class BugsnagService {
         }
 
         // create the bugsnag client
-        def client = new Client(conf.apikey)
+        def bugsnag = new Bugsnag(conf.apikey)
 
         // configure the release stage or set it to the current environment name
-        client.setReleaseStage(conf.releasestage ?: Environment.current.name)
+        bugsnag.setReleaseStage(conf.releasestage ?: Environment.current.name)
 
         // configure the release notify stages
         if (conf.containsKey('notifyreleasestages')) {
-            client.setNotifyReleaseStages(conf.notifyreleasestages)
-        }
-
-        // configure the context of the client
-        if (context) {
-            client.setContext(context)
-        } else if (conf.containsKey('context')) {
-            client.setContext(conf.context)
+            bugsnag.setNotifyReleaseStages(conf.notifyreleasestages)
         }
 
         // set the application version
-        client.setAppVersion(grailsApplication.metadata.getApplicationVersion())
+        bugsnag.setAppVersion(grailsApplication.metadata.getApplicationVersion())
 
-        return client
+        return bugsnag
     }
 
-    def notify( HttpServletRequest request, Exception exception, Map extraMetaData = [:] ) {
+    void notify( HttpServletRequest request, Exception exception, Map extraMetaData = [:] ) {
 
-        def client = getConfiguredClient(request.requestURI)
+        def bugsnag = getConfiguredClient()
 
         try {
-            client.setUserId(request.remoteUser)
-            MetaData metaData = new MetaData()
+            Report report = new Report(bugsnag.config, exception)
+            report.setContext(request.requestURI)
+            report.setUserId(request.remoteUser)
+            //
 
-            metaData.addToTab("app", "application name", grailsApplication.metadata.getApplicationName())
+            report.addToTab("app", "application name", grailsApplication.metadata.getApplicationName())
 
-            metaData.addToTab("environment", "grails version", grailsApplication.metadata.getGrailsVersion())
-            metaData.addToTab("environment", "java version", System.getProperty("java.version"))
-            metaData.addToTab("environment", "java vendor", System.getProperty("java.vendor"))
-            metaData.addToTab("environment", "os name", System.getProperty("os.name"))
-            metaData.addToTab("environment", "os version", System.getProperty("os.version"))
-            metaData.addToTab("environment", "servlet", SCH.servletContext?.serverInfo)
+            report.addToTab("environment", "grails version", grailsApplication.metadata.getGrailsVersion())
+            report.addToTab("environment", "java version", System.getProperty("java.version"))
+            report.addToTab("environment", "java vendor", System.getProperty("java.vendor"))
+            report.addToTab("environment", "os name", System.getProperty("os.name"))
+            report.addToTab("environment", "os version", System.getProperty("os.version"))
+            report.addToTab("environment", "servlet", SCH.servletContext?.serverInfo)
 
-            metaData.addToTab("user", "remoteUser", request.remoteUser ?: "(none)")
-            metaData.addToTab("user", "userPrincipal", request.userPrincipal ?: "(none)")
+            report.addToTab("user", "remoteUser", request.remoteUser ?: "(none)")
+            report.addToTab("user", "userPrincipal", request.userPrincipal ?: "(none)")
 
-            metaData.addToTab("request", "requestURI", request.requestURI)
-            metaData.addToTab("request", "forwardURI", request.forwardURI)
-            metaData.addToTab("request", "cookies", request.cookies.collect {
+            report.addToTab("request", "requestURI", request.requestURI)
+            report.addToTab("request", "forwardURI", request.forwardURI)
+            report.addToTab("request", "cookies", request.cookies.collect {
                 "\nName: ${it.name}\nMax Age: ${it.maxAge}\nPath: ${it.path}\nSecure: ${it.secure}\nDomain: ${it.domain}\nVersion: ${it.version}\nValue: ${it.value}"
             }.join("\n"))
-            metaData.addToTab("request", "headers", request.headerNames.findAll {
+            report.addToTab("request", "headers", request.headerNames.findAll {
                 it != 'cookie'
             }.collect { headerName -> "${headerName}: ${request.getHeaders(headerName).toList()}" }.join('\n'))
-            metaData.addToTab("request", "authType", request.authType)
-            metaData.addToTab("request", "method", request.method)
-            metaData.addToTab("request", "server", request.serverName ?: "(none)")
-            metaData.addToTab("request", "port", request.serverPort ?: "(none)")
-            metaData.addToTab("request", "content type", request.contentType ?: "(none)")
-            metaData.addToTab("request", "character encoding", request.characterEncoding ?: "(none)")
-            metaData.addToTab("request", "scheme", request.scheme ?: "(none)")
-            metaData.addToTab("request", "queryString", request.queryString ?: "(none)")
-            metaData.addToTab("request", "session", request.getSession(false)?.toString())
+            report.addToTab("request", "authType", request.authType)
+            report.addToTab("request", "method", request.method)
+            report.addToTab("request", "server", request.serverName ?: "(none)")
+            report.addToTab("request", "port", request.serverPort ?: "(none)")
+            report.addToTab("request", "content type", request.contentType ?: "(none)")
+            report.addToTab("request", "character encoding", request.characterEncoding ?: "(none)")
+            report.addToTab("request", "scheme", request.scheme ?: "(none)")
+            report.addToTab("request", "queryString", request.queryString ?: "(none)")
+            report.addToTab("request", "session", request.getSession(false)?.toString())
 
-            metaData.addToTab("request", "xml", request.xml?.text())
-            metaData.addToTab("request", "json", request.json?.text())
+            report.addToTab("request", "xml", request.xml?.text())
+            report.addToTab("request", "json", request.json?.text())
 
             extraMetaData.each { k, v ->
-                metaData.addToTab("extra", k, v)
+                report.addToTab("extra", k, v)
             }
 
-            //TODO: get handler for including user defined metadata
-            if (addMetadata instanceof groovy.lang.Closure) {
-                try {
-                    addMetadata(metaData)
-                }
-                catch (excp) {
-                    log.error "error calling 'addMetadata' closure.", excp
-                }
-            }
-
-            client.notify(exception, metaData)
+            bugsnag.notify(report)
 
         } catch (excp) {
             log.error "error calling notify", excp
